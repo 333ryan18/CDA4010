8c0c90fc43e36b8ea81ebf670675574f7efbb6f6
333ryan18 <ryanrukab@gmail.com>
2018-10-02 14:16:30 -0400

    Changes to Criminal Background Table

--- js/main.js
+++ js/main.js
@@ -1,53 +1,17 @@
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-    // Append table with add row form on add new button click
-    $(".add-new").click(function(){
-        $(this).attr("disabled", "disabled");
-        var index = $("table tbody tr:last-child").index();
-        var row = '<tr>' +
-            '<td><input type="date" class="form-control" id="validationTooltipDate" required></td>' +
-            '<td><input type="text" class="form-control" id="validationTooltipTypeCrime" required></td>' +
-            '<td><input type="text" class="form-control" id="validationTooltipDescription" required></td>' +
-            '<td><a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a><a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a><a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a></td>' +
-            '</tr>';
-        $("table").append(row);     
-        $("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-    // Add row on add button click
-    $(document).on("click", ".add", function(){
-        var empty = false;
-        var input = $(this).parents("tr").find('input[type="text"], input[type="date"], :selected');
-        input.each(function(){
-            if(!$(this).val()){
-                $(this).addClass("error");
-                empty = true;
-            } else{
-                $(this).removeClass("error");
-            }
-        });
-        $(this).parents("tr").find(".error").first().focus();
-        if(!empty){
-            input.each(function(){
-                $(this).parent("td").html($(this).val());
-            });         
-            $(this).parents("tr").find(".add, .edit").toggle();
-            $(".add-new").removeAttr("disabled");
-        }       
-    });
-    // Edit row on edit button click
-    $(document).on("click", ".edit", function(){        
-        $(this).parents("tr").find("td:not(:last-child)").each(function(){
-            $(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
-        });     
-        $(this).parents("tr").find(".add, .edit").toggle();
-        $(".add-new").attr("disabled", "disabled");
-    });
-    // Delete row on delete button click
-    $(document).on("click", ".delete", function(){
-        $(this).parents("tr").remove();
-        $(".add-new").removeAttr("disabled");
-        $('.tooltip-inner').hide();
-        $('.arrow').hide();
-    });
+ $(document).ready(function(){
+    var i=1;
+    $("#add_row").click(function(){
+    $('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='date"+i+"' type='date' placeholder='Date' class='form-control' required/> </td><td><select class='custom-select d-block w-100' name='type0' required><option value=''>Choose...</option><option value='Misdemeanor'>Misdemeanor</option><option value='Felony'>Felony</option></select></td><td><input type='text' name='description0' placeholder='Description' class='form-control'/></td><td><select class='custom-select d-block w-100' name='punishment0'required><option value=''>Choose...</option><option value='Confinement'>Confinement</option><option value='Probation'>Probation</option><option value='Probation'>Community Service</option><option value='Felony'>Felony</option></select></td><td><input type='number' class='form-control' id='Years' placeholder='Year(s)' name='punishmentyrs0' required><input type='number' class='form-control' id='Months' placeholder='Month(s)' name='punishmentmth0' required></td>");
+
+    $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+    i++; 
 });
+    $("#delete_row").click(function(){
+       if(i>1){
+       $("#addr"+(i-1)).html('');
+       i--;
+       }
+    });
+
+});
+
